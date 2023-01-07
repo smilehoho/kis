8696e1ddabd0e928703f54f5183f394d550f2134
Wooseok <smilehoho@gmail.com>
2023-01-07 21:46:12 +0900

    feat: logging-requests

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
-import requests
 from config import Config
+from helper.logging_requests import session
 
 
 class Api:
@@ -11,10 +11,11 @@ class Api:
         self.access_token: str = ""
 
     def approval(self) -> str | None:
-        """실시간 (웹소켓) 접속키 발급받으실 수 있는 API 입니다.
+        """
+        실시간 (웹소켓) 접속키 발급받으실 수 있는 API 입니다.
         웹소켓 이용 시 해당 키를 appkey와 appsecret 대신 헤더에 넣어 API를 호출합니다.
         """
-        response = requests.post(
+        response = session.post(
             url=self.config.DOMAIN + "/oauth2/Approval",
             headers={"Content-Type": "application/json; utf-8"},
             json={
@@ -30,11 +31,12 @@ class Api:
         return self.approval_key
 
     def hashkey(self, contents: dict) -> str:
-        """해쉬키(Hashkey)는 보안을 위한 요소로 사용자가 보낸 요청 값을 중간에 탈취하여 변조하지 못하도록 하는데 사용됩니다.
+        """
+        해쉬키(Hashkey)는 보안을 위한 요소로 사용자가 보낸 요청 값을 중간에 탈취하여 변조하지 못하도록 하는데 사용됩니다.
         보안을 위해 POST로 보내는 요청(주로 주문/정정/취소 API 해당)은 사전에 body 값 암호화가 필요하며,
         이 때 hashkey를 활용한 암호화를 진행합니다.
         """
-        response = requests.post(
+        response = session.post(
             url=self.config.DOMAIN + "/uapi/hashkey",
             headers={
                 "Content-Type": "application/json; utf-8",
@@ -50,7 +52,7 @@ class Api:
 
     def tokenp(self) -> str:
         """본인 계좌에 필요한 인증 절차로, 인증을 통해 접근 토큰을 부여받아 오픈API 활용이 가능합니다."""
-        response = requests.post(
+        response = session.post(
             url=self.config.DOMAIN + "/oauth2/tokenP",
             headers={
                 "Content-Type": "application/json; utf-8",
--- app/main.py
+++ app/main.py
@@ -1,32 +1,21 @@
 import logging
-from http.client import HTTPConnection
 
 from api import Api
 from config import DevConfig as Config
 from dotenv import dotenv_values
 
-
-def init_logging():
-    """https://stackoverflow.com/questions/16337511/log-all-requests-from-the-python-requests-module"""
-    HTTPConnection.debuglevel = 1
-
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-
-    request_log = logging.getLogger("requests.packages.urllib3")
-    request_log.setLevel(logging.DEBUG)
-    request_log.propagate = True
+logger = logging.getLogger(__name__)
 
 
 def main():
     env = dotenv_values(".env")
-    api = Api(appkey=env["appkey"], secretkey=env["secretkey"], config=Config)
+    api = Api(appkey=env["appkey"], secretkey=env["secretkey"], config=Config)  # noqa
 
-    api.approval()
-    # print(api.hashkey(contents={"STRING": "1234", "INT": 1234}))
-    # print(api.tokenp())
+    # logger.debug(api.approval())
+    # logger.debug(api.hashkey(contents={"STRING": "1234", "INT": 1234}))
+    # logger.debug(api.tokenp())
 
 
 if __name__ == "__main__":
-    init_logging()
+    logging.basicConfig(level=logging.DEBUG)
     main()
